b1eee39919717a16b37019699a521a3cd21931e2
Zeus6768 <akaps6768@gmail.com>
2024-01-13 13:55:53 +0900

    test: 1.6 돌아온 '모두를 위한 평등'

---
+++ src/main/java/money/Money.java
@@ -0,0 +1,2 @@
+package money;public class Money {
+}
--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -25,5 +25,7 @@ public class MoneyTest {
     void testEquality() {
         assertEquals(new Dollar(5), new Dollar(5));
         assertNotEquals(new Dollar(5), new Dollar(6));
+        assertEquals(new Franc(5), new Franc(5));
+        assertNotEquals(new Franc(5), new Franc(6));
     }
 }
