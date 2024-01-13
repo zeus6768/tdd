c81821409d4aabd0a428f20f577c15b4836d9169
Zeus6768 <akaps6768@gmail.com>
2024-01-13 13:45:27 +0900

    feat: 1.5 솔직히 말하자면

---
+++ src/main/java/money/Franc.java
@@ -0,0 +1,20 @@
+package money;
+
+public class Franc {
+
+    private int amount;
+
+    Franc(int amount) {
+        this.amount = amount;
+    }
+
+    Franc times(int multiplier) {
+        return new Franc(amount * multiplier);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        Franc franc = (Franc) object;
+        return amount == franc.amount;
+    }
+}
