fa47acc9782eb0d75f01e82fa30247cb8c7de488
Zeus6768 <akaps6768@gmail.com>
2024-01-13 14:11:11 +0900

    feat: 1.8 객체 만들기

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -1,8 +1,18 @@
 package money;
 
-public class Money {
+public abstract class Money {
 
     protected int amount;
+
+    static Dollar dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    static Franc franc(int amount) {
+        return new Franc(amount);
+    }
+
+    abstract Money times(int multiplier);
 
     @Override
     public boolean equals(Object object) {
