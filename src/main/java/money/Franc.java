55beb59a92f743c73585cb91b999c4ca0dc0e71c
Zeus6768 <akaps6768@gmail.com>
2024-01-13 13:56:19 +0900

    feat: 1.6 돌아온 '모두를 위한 평등'

--- src/main/java/money/Franc.java
+++ src/main/java/money/Franc.java
@@ -1,8 +1,6 @@
 package money;
 
-public class Franc {
-
-    private int amount;
+public class Franc extends Money {
 
     Franc(int amount) {
         this.amount = amount;
@@ -10,11 +8,5 @@ public class Franc {
 
     Franc times(int multiplier) {
         return new Franc(amount * multiplier);
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        Franc franc = (Franc) object;
-        return amount == franc.amount;
     }
 }
--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -1,2 +1,12 @@
-package money;public class Money {
+package money;
+
+public class Money {
+
+    protected int amount;
+
+    @Override
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount;
+    }
 }
