b493ba5def87691a7bd70a3d10905c308b90ce7b
Zeus6768 <akaps6768@gmail.com>
2024-01-13 13:26:10 +0900

    feat: 1.1 다중 통화를 지원하는 Money 객체

---
+++ src/main/java/money/Dollar.java
@@ -0,0 +1,14 @@
+package money;
+
+public class Dollar {
+
+    int amount;
+
+    Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    void times(int multiplier) {
+        amount *= multiplier;
+    }
+}
