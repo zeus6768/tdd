5fb30fcb16ebf6660d6ecd9a86ee9498001b6991
Zeus6768 <akaps6768@gmail.com>
2024-01-15 16:57:14 +0900

    test: 1.11 모든 악의 근원

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -15,18 +15,9 @@ public class MoneyTest {
     }
 
     @Test
-    void testFrancMultiplication() {
-        Franc five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-    }
-
-    @Test
     void testEquality() {
         assertEquals(Money.dollar(5), Money.dollar(5));
         assertNotEquals(Money.dollar(5), Money.dollar(6));
-        assertEquals(Money.franc(5), Money.franc(5));
-        assertNotEquals(Money.franc(5), Money.franc(6));
         assertNotEquals(Money.franc(5), Money.dollar(5));
     }
 
@@ -34,10 +25,5 @@ public class MoneyTest {
     void testCurrency() {
         assertEquals("USD", Money.dollar(1).currency());
         assertEquals("CHF", Money.franc(1).currency());
-    }
-
-    @Test
-    void testDifferentClassEquality() {
-        assertEquals(new Money(10, "CHF"), new Franc(10, "CHF"));
     }
 }
