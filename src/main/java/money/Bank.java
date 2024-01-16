b21888f8141c43784b029367c5d94c16c1637a83
Zeus6768 <akaps6768@gmail.com>
2024-01-16 09:17:31 +0900

    feat: 1.13 진짜로 만들기(2)

--- src/main/java/money/Bank.java
+++ src/main/java/money/Bank.java
@@ -2,6 +2,7 @@ package money;
 
 public class Bank {
     Money reduce(Expression source, String to) {
-        return Money.dollar(10);
+        Sum sum = (Sum) source;
+        return sum.reduce(to);
     }
 }
