1ee3195844c89764d1c8986c693c7d5d1826807a
Zeus6768 <akaps6768@gmail.com>
2024-01-16 10:18:17 +0900

    feat: 1.14 바꾸기(1)

--- src/main/java/money/Bank.java
+++ src/main/java/money/Bank.java
@@ -1,7 +1,20 @@
 package money;
 
+import java.util.Hashtable;
+
 public class Bank {
+
+    private Hashtable<Pair, Integer> rates = new Hashtable<>();
+
     Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
+    }
+
+    int rate(String from, String to) {
+        return rates.get(new Pair(from, to));
+    }
+
+    void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), rate);
     }
 }
--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -30,8 +30,9 @@ public class Money implements Expression {
         return new Sum(this, addend);
     }
 
-    public Money reduce(String to) {
-        return this;
+    public Money reduce(Bank bank, String to) {
+        int rate = bank.rate(currency, to);
+        return new Money(amount / rate, to);
     }
 
     @Override
